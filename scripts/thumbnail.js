a41cc3de1f2518686a76ecb0650c08778656ae41
magicianShiro <magician_shiro@hotmail.com>
2018-01-22 23:07:44 +0800

    添加了博客运行时间

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -12,7 +12,9 @@ hexo.extend.helper.register('thumbnail', function (post) {
   target = post.thumbnail || post.banner || ''
   // 判断banner图片是否是网络图片，如果是网络图片直接使用否者加上根路径
   if (post.banner) {
-    var banner = this.url_for(post.path)  +  target
+    let rootPath = this.url_for(post.path).split('.')[0]
+    let lostChar = rootPath.slice(-1)
+    banner = lostChar === '/' ? rootPath + target : rootPath + '/' +target
     if (target.indexOf('http') > 0 || /^\/\//.test(target)) {
       banner = target
     }
